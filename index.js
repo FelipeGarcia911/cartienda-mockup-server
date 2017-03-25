5ebb9090acd31feaee8d086890387a980718d164
Felipe Garcia <felipe.codigoriginal.com>
2017-03-25 00:29:55 -0500

    Landing Page

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Load Libraries
 // -------------------------------------------------------------------------------------------------------------------
+
 var bodyParser      = require('body-parser');
 var methodOverride  = require("method-override");
 var path            = require('path');
@@ -12,13 +13,26 @@ var http    = require('http').Server(app);
 // Variables
 // -------------------------------------------------------------------------------------------------------------------
 var port    = process.env.PORT || 3000;
+var main_route = __dirname + '/public/web/';
 // -------------------------------------------------------------------------------------------------------------------
 
 // App Configurations
 // -------------------------------------------------------------------------------------------------------------------
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/images', express.static(__dirname + '/public/web/images'));
+app.use('/css', express.static(__dirname + '/public/web/css'));
+app.use('/js', express.static(__dirname + '/public/web/js'));
 app.use(bodyParser.json());
 app.use(methodOverride());
+// -------------------------------------------------------------------------------------------------------------------
+
+// Template Route
+// -------------------------------------------------------------------------------------------------------------------
+var router = express.Router();
+router.get('/', function(req, res) {
+    res.sendFile(main_route + 'index.html');
+});
+app.use(router);
 // -------------------------------------------------------------------------------------------------------------------
 
 // Index - Route
@@ -35,4 +49,4 @@ app.use('/cartienda_service/api', cartienda_service);
 http.listen(port, function() {
     console.log('Our app is running on http://localhost:' + port);
 });
-// -------------------------------------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------------------
